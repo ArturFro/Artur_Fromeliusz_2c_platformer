7afa765d48732dbe41811f1b029bdc69ef50cfec
ArturFro <fromeliusz@wp.pl>
2026-09-24 18:58:01 +0200

    movement

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+    [SerializeField] private GameObject player;
     [SerializeField] private Rigidbody2D rig;
     [SerializeField] private float jumpForce;
     [SerializeField] private bool onGround;
+    [SerializeField] private Collider2D groundCol;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float moveSpeed;
 
 
     void Start()
@@ -22,9 +25,26 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space) && onGround)
         {
-            print("jump");
+            
             rig.velocity = new UnityEngine.Vector2(rig.velocity.x, jumpForce);
+            onGround = false;  
             
         }   
+
+        horizontalInput = Input.GetAxis("Horizontal");
     }
+
+    private void OnCollisionEnter2D(Collision2D player)
+    {
+        if(player.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        rig.velocity = new UnityEngine.Vector2(horizontalInput * moveSpeed, rig.velocity.y);
+    }
+
+
 }
